0f6a131403c530d99775f9d11356243f817ba0b1
Paul-M-Coorales <paulmatiascorrales@gmail.com>
2022-08-02 14:34:45 -0300

    interactuando con html

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ for (let i = numero; i <= 10; i++) {
     console.log ("Tu numero es: " + i);
 } */
 
-/* SIMULADOR INTERACTIVO*/
+/* /* SIMULADOR INTERACTIVO*/
 
 // SIMULADOR DEL LADO DEL VENDEDOR
-const ProductosAvender = [];
+/*   const ProductosAvender = [];
 
 function IngresarProductos() {
   class producto {
@@ -42,6 +42,7 @@ function IngresarProductos() {
   );
 }
 
+
 // SIMULADOR DEL LADO DEL COMPRADOR
 
 class item {
@@ -49,7 +50,7 @@ class item {
     this.id = elemento.id;
     this.nombre = elemento.nombre;
     this.precio = elemento.precio;
-  }
+  } 
 }
 const carrito = [];
 
@@ -60,13 +61,14 @@ function BuscarProducto(id) {
   return ProductosAvender.find((elemento) => elemento.id == id);
 }
 
+
 function Agregar() {
   let nombre = prompt(
     "Hola, bienvenido a Soldimet Repuestos, por favor ingresa tu nombre"
   );
-
+  
   let mail = prompt("Escribe tu Email");
-
+  
   alert("Bienvenido " + nombre + "!!");
   let salida =
     "Elige el numero del producto deseado. Cancelar para salir: \n\n";
@@ -87,20 +89,15 @@ function Agregar() {
     let producto = BuscarProducto(id_item);
     console.log(producto);
     CargarEnCarrito(producto);
-  }
+  } 
   let pago = "Los productos elegidos son : \n\n";
   let total = 0;
   for (let producto_carrito of carrito) {
     let producto = new item(producto_carrito);
     total += parseInt(producto.precio);
-    pago +=
-      producto_carrito.id +
-      "=>" +
-      producto_carrito.nombre +
-      "  $" +
-      producto_carrito.precio +
-      "\n";
-    alert(pago);
+    pago+=
+      producto_carrito.id + "=>" + producto_carrito.nombre + "  $" + producto_carrito.precio + "\n";
+      alert(pago);
   }
   alert(
     nombre +
@@ -140,6 +137,124 @@ function Agregar() {
       break;
   }
 }
+IngresarProductos();
+Agregar(); */
+
+
+// INTERACTUAR CON HTML (PAGINA JUNTAS.HTML)
+
+const productos = [
+  {id:1, nombre:"Junta ford", precio:3000,imagen:"juntas.jpg"},
+  {id:2, nombre:"Junta fiat", precio:3400, imagen:"juntas.jpg"},
+  {id:3, nombre:"Junta chevrolet", precio:3700,imagen:"juntas.jpg"},
+  {id:4, nombre:"Junta renault", precio:3900, imagen:"juntas.jpg"},
+];
+
+class item {
+  constructor(elemento) {
+    this.id = elemento.id;
+    this.nombre = elemento.nombre;
+    this.precio = elemento.precio;
+    this.imagen = elemento.imagen
+  }
+}
+const carrito = [];
+
+function CargarEnCarrito(item) {
+  carrito.push(item);
+}
+function BuscarProducto(id) {
+  return productos.find((elemento) => elemento.id == id);
+}
+
+function Agregar() {
+  let nombre = prompt(
+    "Hola, bienvenido a Soldimet Repuestos, por favor ingresa tu nombre"
+  );
+
+  let mail = prompt("Escribe tu Email");
+
+  alert("Bienvenido " + nombre + "!!");
+  let salida =
+    "Elige el numero del producto deseado. Cancelar para salir: \n\n";
+  for (let item of productos) {
+    salida += item.id + "=>" + item.nombre + "  $" + item.precio + "\n";
+  }
+    
+  let id_item = 0;
+
+  while (id_item != null) {
+    let id_item = prompt(salida);
+
+    if (id_item != null) {
+      id_item = parseInt(id_item);
+    } else {
+      break;
+    }
+
+   
+    
+    let producto = BuscarProducto(id_item);
+    console.log(producto);
+    CargarEnCarrito(producto);
+    
+  }
+  let pago = "";
+  let total = 0;
+  for (let producto_carrito of carrito) {
+    let producto = new item(producto_carrito);
+    total += parseInt(producto.precio);
+    pago +=` <div class ="container"> 
+    <h1>Compraste este articulo</h1><br>
+    <p><b>${producto_carrito.id}</b><br>
+    <img src="../img/${producto_carrito.imagen}" alt=""width="300"><br>
+    <b>${producto_carrito.nombre}</b>
+    <b>$${producto_carrito.precio}</b></p>
+    </div> `
+
+  }
+  document.getElementById("productos").innerHTML=pago;
+
+  alert(
+    nombre +
+      " Tu pedido se a realizado con exito!! La factura fue enviada a " +
+      mail +
+      " Tu total a pagar es de $ " +
+      total +
+      " IVA inluido"
+  );
+
+  let compra = prompt("Como vas a pagar? debito o credito?");
+  let cuotas = "credito";
+  if (compra == "debito") {
+    return alert("1 cuota de " + total);
+  } else if (compra == "credito") {
+    cuotas = parseInt(
+      prompt(
+        "Indica la cantidad de cuotas, tienes para elegir entre 1, 3, 6, 9 o 12"
+      )
+    );
+  }
+  switch (cuotas) {
+    case (cuotas = 1):
+      alert("tu pago es 1 cuota de " + total);
+      break;
+    case (cuotas = 3):
+      alert("tu pago es 3 cuota de " + total / 3);
+      break;
+    case (cuotas = 6):
+      alert("tu pago es 6 cuota de " + total / 6);
+      break;
+    case (cuotas = 9):
+      alert("tu pago es 9 cuota de " + total / 9);
+      break;
+    case (cuotas = 12):
+      alert("tu pago es 12 cuota de " + total / 12);
+      break;
+  }
+}
+
 
 IngresarProductos();
-Agregar();
+Agregar(); 
+
